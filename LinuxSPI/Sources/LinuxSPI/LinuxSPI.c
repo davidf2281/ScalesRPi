510263f0978c4901998f5f15d96ebc97371ab046
David Fearon <david@davidfearon.com>
2023-11-02 18:53:26 +0000

    Try new library

--- LinuxSPI/Sources/LinuxSPI/LinuxSPI.c
+++ LinuxSPI/Sources/LinuxSPI/LinuxSPI.c
@@ -1,54 +1,110 @@
+/*
+ *
+ * This file is based on  of pyA20.
+ * spi_lib.c python SPI extension.
+ *
+ * Copyright (c) 2014 Stefan Mavrodiev @ OLIMEX LTD, <support@olimex.com>
+ *
+ * pyA20 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * Adpated by Philippe Van Hecke <lemouchon@gmail.com>
+ */
 
-/* CREDIT: Adapted from https://github.com/doceme/py-spidev/blob/master/spidev_module.c */
-#include <stdint.h>
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdint.h>
 #include <fcntl.h>
-#include <linux/spi/spidev.h>
-#include <linux/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <sys/ioctl.h>
-#include <linux/ioctl.h>
+#include <string.h>
 
-#define SPIDEV_MAXPATH 4096
+#include <linux/spi/spidev.h>
 
-#define BLOCK_SIZE_CONTROL_FILE "/sys/module/spidev/parameters/bufsiz"
-// The xfwr3 function attempts to use large blocks if /sys/module/spidev/parameters/bufsiz setting allows it.
-// However where we cannot get a value from that file, we fall back to this safe default.
-#define XFER3_DEFAULT_BLOCK_SIZE SPIDEV_MAXPATH
-// Largest block size for xfer3 - even if /sys/module/spidev/parameters/bufsiz allows bigger
-// blocks, we won't go above this value. As I understand, DMA is not used for anything bigger so why bother.
-#define XFER3_MAX_BLOCK_SIZE 65535
+#include "spidev_lib.h"
 
-// Maximum block size for xfer3
-// Initialised once by get_xfer3_block_size
-uint32_t xfer3_block_size = 0;
+int spi_open(char *device, spi_config_t config) {
+    int fd;
 
-// Read maximum block size from the /sys/module/spidev/parameters/bufsiz
-// In case of any problems reading the number, we fall back to XFER3_DEFAULT_BLOCK_SIZE.
-// If number is read ok but it exceeds the XFER3_MAX_BLOCK_SIZE, it will be capped to that value.
-// The value is read and cached on the first invocation. Following invocations just return the cached one.
-uint32_t get_xfer3_block_size(void) {
-	int value;
+    /* Open block device */
+    fd = open(device, O_RDWR);
+    if (fd < 0) {
+        return fd;
+    }
 
-	// If value was already initialised, just use it
-	if (xfer3_block_size != 0) {
-		return xfer3_block_size;
-	}
+    /* Set SPI_POL and SPI_PHA */
+    if (ioctl(fd, SPI_IOC_WR_MODE, &config.mode) < 0) {
+        return -1;
+    }
+    if (ioctl(fd, SPI_IOC_RD_MODE, &config.mode) < 0) {
+        return -1;
+    }
 
-	// Start with the default
-	xfer3_block_size = XFER3_DEFAULT_BLOCK_SIZE;
+    /* Set bits per word*/
+    if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &config.bits_per_word) < 0) {
+        return -1;
+    }
+    if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &config.bits_per_word) < 0) {
+        return -1;
+    }
 
-	FILE *file = fopen(BLOCK_SIZE_CONTROL_FILE,"r");
-	if (file != NULL) {
-		if (fscanf(file, "%d", &value) == 1 && value > 0) {
-			if (value <= XFER3_MAX_BLOCK_SIZE) {
-				xfer3_block_size = value;
-			} else {
-				xfer3_block_size = XFER3_MAX_BLOCK_SIZE;
-			}
-		}
-		fclose(file);
-	}
+    /* Set SPI speed*/
+    if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &config.speed) < 0) {
+        return -1;
+    }
+    if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &config.speed) < 0) {
+        return -1;
+    }
 
-	return xfer3_block_size;
+    /* Return file descriptor */
+    return fd;
+}
+
+int spi_close(int fd) {
+    return close(fd);
+}
+
+int spi_xfer(int fd, uint8_t *tx_buffer, uint8_t tx_len, uint8_t *rx_buffer, uint8_t rx_len){
+    struct spi_ioc_transfer spi_message[1];
+    memset(spi_message, 0, sizeof(spi_message));
+    
+    spi_message[0].rx_buf = (unsigned long)rx_buffer;
+    spi_message[0].tx_buf = (unsigned long)tx_buffer;
+    spi_message[0].len = tx_len;
+    
+    return ioctl(fd, SPI_IOC_MESSAGE(1), spi_message);
+}
+
+int spi_read(int fd, uint8_t *rx_buffer, uint8_t rx_len){
+    struct spi_ioc_transfer spi_message[1];
+    memset(spi_message, 0, sizeof(spi_message));
+    
+    spi_message[0].rx_buf = (unsigned long)rx_buffer;
+    spi_message[0].len = rx_len;
+    
+    
+    return ioctl(fd, SPI_IOC_MESSAGE(1), spi_message);
+}
+
+int spi_write(int fd, uint8_t *tx_buffer, uint8_t tx_len){
+    struct spi_ioc_transfer spi_message[1];
+    memset(spi_message, 0, sizeof(spi_message));
+    
+    spi_message[0].tx_buf = (unsigned long)tx_buffer;
+    spi_message[0].len = tx_len;
+    
+    return ioctl(fd, SPI_IOC_MESSAGE(1), spi_message);
 }
