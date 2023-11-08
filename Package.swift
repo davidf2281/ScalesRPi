391ec6cfaf582ecba46dfb89b22c75d38073dc1c
David Fearon <david@davidfearon.com>
2023-11-08 20:19:25 +0000

    Downgrade SwiftyGPIO to v1.1.14 to fix 'Message too long' error

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/davidf2281/ScalesCore.git", branch: "main"),
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0")
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", exact: "1.1.14")
     ],
     targets: [
         .executableTarget(
