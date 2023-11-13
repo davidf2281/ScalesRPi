6147bbdab4f3be1ccc3595157ab601463bb7ad0b
David Fearon <david@davidfearon.com>
2023-11-13 19:31:36 +0000

    my -> ml

--- Sources/ScalesRPi/ST7789/ST7789.swift
+++ Sources/ScalesRPi/ST7789/ST7789.swift
@@ -31,7 +31,7 @@ struct ST7789 {
         self.sendCommands([
             SWRESET(),
             COLMOD(bpp: self.bpp),
-            MADCTL([.mx, .my]),
+            MADCTL([.mx, .ml]),
             INVON(),
             SLPOUT(),
             DISPON()
