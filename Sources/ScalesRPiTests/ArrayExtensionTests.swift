aca04371fb3fc7775fd507d4c25e38c51a33e99d
David Fearon <david@davidfearon.com>
2023-11-14 19:28:19 +0000

    Update ArrayExtensionTests.swift

--- Sources/ScalesRPiTests/ArrayExtensionTests.swift
+++ Sources/ScalesRPiTests/ArrayExtensionTests.swift
@@ -5,8 +5,8 @@ import XCTest
 final class ArrayExtensionTests: XCTestCase {
 
     func testArraySplit() throws {
-        let dummyData: [UInt8] = .init(repeating: 128, count: 9000)
-        let result = dummyData.split(4096)
+        let mockData: [UInt8] = .init(repeating: 128, count: 9000)
+        let result = mockData.split(4096)
         XCTAssertEqual(result.count, 3)
         XCTAssertEqual(result[0].count, 4096)
         XCTAssertEqual(result[1].count, 4096)
@@ -14,8 +14,8 @@ final class ArrayExtensionTests: XCTestCase {
     }
     
     func testToUInt8() throws {
-        let dummyData: [UInt16] = [0xC4F9]
-        let result = dummyData.toUInt8
+        let mockData: [UInt16] = [0xC4F9]
+        let result = mockData.toUInt8
         XCTAssertEqual(result.count, 2)
         XCTAssertEqual(result[0], 0xC4)
         XCTAssertEqual(result[1], 0xF9)
