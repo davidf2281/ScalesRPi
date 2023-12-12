294f8347ee2e82dbd350c56dd7a6902b039960ba
David Fearon <david@davidfearon.com>
2023-12-12 18:57:51 +0000

    Revert commented line to test memory-leak fix

--- Sources/ScalesRPi/DS18B20Sensor.swift
+++ Sources/ScalesRPi/DS18B20Sensor.swift
@@ -36,9 +36,9 @@ class DS18B20Sensor: ScalesCore.Sensor {
     func start() {
         self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
             if let self {
-//                guard let reading = getReading() else {
-//                    return
-//                }
+                guard let reading = getReading() else {
+                    return
+                }
                 
                 Task {
                     await self.delegate?.didGetReading(0.2)
