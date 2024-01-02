e06c6972fe6dc5442fb71229941a850ed247dfe9
David Fearon <david@davidfearon.com>
2024-01-02 10:59:13 +0000

    Get an i2c transaction working; implement and partially refactor reading compensation algos from BME280 datasheet

--- Sources/ScalesRPi/BME280Sensor.swift
+++ Sources/ScalesRPi/BME280Sensor.swift
@@ -15,7 +15,7 @@ final class BME280Sensor: ScalesCore.Sensor {
     let location: ScalesCore.SensorLocation
     
     private let slaveID: Int = 0x76
-    private let sensorIDRegisterAddress: UInt8 = 0xD0
+    private let IDRegisterAddress: UInt8 = 0xD0
     
     private let minUpdateInterval: TimeInterval
     private let i2c: I2CInterface
@@ -103,7 +103,7 @@ final class BME280Sensor: ScalesCore.Sensor {
         self.location = location
         self.minUpdateInterval = minUpdateInterval
         
-        i2c.writeByte(slaveID, value: sensorIDRegisterAddress)
+        i2c.writeByte(slaveID, value: IDRegisterAddress)
         let sensorID = i2c.readByte(slaveID)
         
         print("BME280 sensor id: \(sensorID)")
@@ -131,45 +131,3 @@ final class BME280Sensor: ScalesCore.Sensor {
         return [Reading(outputType: .temperature(unit: .celsius), sensorLocation: self.location, sensorID: self.id, value: 0)]
     }
 }
-
- /*
- // Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8
- fractional bits).
- // Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
- BME280_U32_t BME280_compensate_P_int64(BME280_S32_t adc_P)
- {
- BME280_S64_t var1, var2, p;
- var1 = ((BME280_S64_t)t_fine) – 128000;
- var2 = var1 * var1 * (BME280_S64_t)dig_P6;
- var2 = var2 + ((var1*(BME280_S64_t)dig_P5)<<17);
- var2 = var2 + (((BME280_S64_t)dig_P4)<<35);
- var1 = ((var1 * var1 * (BME280_S64_t)dig_P3)>>8) + ((var1 * (BME280_S64_t)dig_P2)<<12);
- var1 = (((((BME280_S64_t)1)<<47)+var1))*((BME280_S64_t)dig_P1)>>33;
- if (var1 == 0)
- {
- return 0; // avoid exception caused by division by zero
- }
- p = 1048576-adc_P;
- p = (((p<<31)-var2)*3125)/var1;
- var1 = (((BME280_S64_t)dig_P9) * (p>>13) * (p>>13)) >> 25;
- var2 = (((BME280_S64_t)dig_P8) * p) >> 19;
- p = ((p + var1 + var2) >> 8) + (((BME280_S64_t)dig_P7)<<4);
- return (BME280_U32_t)p;
- }
- 
- // Returns humidity in %RH as unsigned 32 bit integer in Q22.10 format (22 integer and 10
- fractional bits).
- // Output value of “47445” represents 47445/1024 = 46.333 %RH
- BME280_U32_t bme280_compensate_H_int32(BME280_S32_t adc_H)
- {
- BME280_S32_t v_x1_u32r;
- v_x1_u32r = (t_fine – ((BME280_S32_t)76800));
- Bosch Sensortec | BME280 Data sheet 26 | 60
- Modifications reserved | Data subject to change without notice Document number: BST-BME280-DS001-23 Revision_1.23_012022
- v_x1_u32r = (((((adc_H << 14) – (((BME280_S32_t)dig_H4) << 20) – (((BME280_S32_t)dig_H5) * v_x1_u32r)) + ((BME280_S32_t)16384)) >> 15) * (((((((v_x1_u32r * ((BME280_S32_t)dig_H6)) >> 10) * (((v_x1_u32r * ((BME280_S32_t)dig_H3)) >> 11) + ((BME280_S32_t)32768))) >> 10) + ((BME280_S32_t)2097152)) * ((BME280_S32_t)dig_H2) + 8192) >> 14));
- v_x1_u32r = (v_x1_u32r – (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((BME280_S32_t)dig_H1)) >> 4));
- v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
- v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
- return (BME280_U32_t)(v_x1_u32r>>12);
- }
- */
--- Sources/ScalesRPiC/include/ScalesRPiC.h
+++ Sources/ScalesRPiC/include/ScalesRPiC.h
@@ -2,5 +2,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 typedef int32_t BME280_S32_t;
+typedef int64_t BME280_S64_t;
+typedef uint64_t BME280_U32_t;
 
-BME280_S32_t BME280_compensate_T_int32(BME280_S32_t adc_T, unsigned short dig_T1, signed short dig_T2, signed short dig_T3);
+BME280_S32_t t_fine(BME280_S32_t adc_T, unsigned short dig_T1, signed short dig_T2, signed short dig_T3);
+BME280_S32_t BME280_compensate_T_int32(BME280_S32_t t_fine);
